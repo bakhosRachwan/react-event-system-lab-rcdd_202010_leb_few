0a2967cc8dd31ef8d958770ef894913476873a72
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2021-01-05 06:45:27 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
-// Code EyesOnMe Component Here
+import Reacr from 'react';
+
+class EyesOnMe extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onFocus={'Good!'} onBlur={"Hey! Eyes on me!"}></button>
+      </div>
+    )
+  }
+}
