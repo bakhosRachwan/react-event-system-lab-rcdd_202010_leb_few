927933ae4403b9e1fbe9f092dc9e41201b9d4ece
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2021-01-05 06:34:12 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,10 +1,11 @@
 import React, {components} from 'react';
 
 export default function Keypad () {
-  render ()
+  render (){
     return (
       <div calssName="keypad">
         <input type="password" >
-      </div>);
-
+      </div>
+    );
+}
 }
